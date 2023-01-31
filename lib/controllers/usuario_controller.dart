a7871b100862a5775b5c9e048ca9f6c524f0b9aa
Jamilyasbarb <mily.yasbarb@gmail.com>
2023-01-31 17:28:18 -0300

    feat: controller usuario
    adicionado controller usuario para implementar a logica da aplicacao

---
+++ lib/controllers/usuario_controller.dart
@@ -0,0 +1,9 @@
+import 'package:compras_vita_health/models/produto_model.dart';
+import 'package:flutter/cupertino.dart';
+
+class UsuarioController extends ChangeNotifier{
+  
+  addProduto(ProdutoModel produto){
+    
+  }
+}
