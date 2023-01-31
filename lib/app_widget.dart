bbdd99e6da328623c60f5719419d72f1eb5d06af
Jamilyasbarb <mily.yasbarb@gmail.com>
2023-01-31 17:21:13 -0300

    feat: rota e peso de fonte
    adicionado rota para a pagina de cadastro de produto e peso da fonte no theme do app

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,3 +1,4 @@
+import 'package:compras_vita_health/pages/cadastro/cadastro_page.dart';
 import 'package:compras_vita_health/pages/home/home_page.dart';
 import 'package:flutter/material.dart';
 
@@ -11,17 +12,23 @@ class AppWidget extends StatelessWidget {
         colorScheme: const  ColorScheme.light(
           primary: Color.fromRGBO(255, 68, 56, 1),
           secondary: Color.fromRGBO(64, 64, 65, 0),
+          
         ),
         textTheme: const TextTheme(
           titleLarge: TextStyle(
+            fontWeight: FontWeight.w700,
             fontFamily: 'MontserratAlternates'
           ),
         ),
         fontFamily: 'MontserratAlternates',
+        buttonTheme: ButtonThemeData(
+          buttonColor: Colors.black
+        )
       ),
       initialRoute: '/',
       routes: {
         '/':(context) => HomePage(),
+        'cadastro':(context) => CadastroPage(),
       },
     );
   }
