46d8cf6fbb00d22580cbc2e4c6773d7658e93bb8
Jamilyasbarb <mily.yasbarb@gmail.com>
2023-02-01 17:03:43 -0300

    feat: provider
    adicionado provider produtoController

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,35 +1,38 @@
+import 'package:compras_vita_health/controllers/produto_controller.dart';
 import 'package:compras_vita_health/pages/cadastro/cadastro_page.dart';
 import 'package:compras_vita_health/pages/home/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class AppWidget extends StatelessWidget {
   const AppWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        colorScheme: const  ColorScheme.light(
-          primary: Color.fromRGBO(255, 68, 56, 1),
-          secondary: Color.fromRGBO(64, 64, 65, 0),
-          
-        ),
-        textTheme: const TextTheme(
-          titleLarge: TextStyle(
-            fontWeight: FontWeight.w700,
-            fontFamily: 'MontserratAlternates'
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => ProdutoController(),)
+      ],
+      child: MaterialApp(
+        theme: ThemeData(
+          colorScheme: const  ColorScheme.light(
+            primary: Color.fromRGBO(255, 68, 56, 1),
+            secondary: Color.fromRGBO(64, 64, 65, 0),
           ),
+          textTheme: const TextTheme(
+            titleLarge: TextStyle(
+              fontWeight: FontWeight.w700,
+              fontFamily: 'MontserratAlternates'
+            ),
+          ),
+          fontFamily: 'MontserratAlternates',
         ),
-        fontFamily: 'MontserratAlternates',
-        buttonTheme: ButtonThemeData(
-          buttonColor: Colors.black
-        )
+        initialRoute: '/',
+        routes: {
+          '/':(context) => HomePage(),
+          'cadastro':(context) => CadastroPage(),
+        },
       ),
-      initialRoute: '/',
-      routes: {
-        '/':(context) => HomePage(),
-        'cadastro':(context) => CadastroPage(),
-      },
     );
   }
 }
