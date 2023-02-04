7b05eedf86cec6b8d71d67bdebf3f6c6fcb3ed7d
Jamilyasbarb <mily.yasbarb@gmail.com>
2023-02-04 16:03:58 -0300

    feat: orientacao de tela
    adicionada configuraracao para tela horizontal e vertical

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:compras_vita_health/app_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.landscapeLeft,
+  ]).then((value) => runApp(AppWidget()));
   runApp(const AppWidget());
 }
 
