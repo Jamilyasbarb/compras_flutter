0b84f1bc34ac3808f71febfc9293131513a6c52e
Jamilyasbarb <mily.yasbarb@gmail.com>
2023-02-04 16:08:04 -0300

    fix: excluir e adquirir
    adicionado logica para excluir e adquirir um novo produto, e alterei a lógica de receber a lista da api

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -25,15 +25,19 @@ class _HomePageState extends State<HomePage> {
     super.initState();
     getListaProduto();
     getUsuario();
-    cadastrado = List<bool>.filled(listaProdutos.length, false);
   }
 
   getListaProduto()async{
     produtos = await ProdutoService().getProdutos();
     if(produtos != null){
+      if(listaProdutos.isEmpty){
+        for (var i = 0; i < produtos!.length; i++) {
+          listaProdutos.add(produtos![i]);
+        }
+      }
       setState(() {
-        listaProdutos = produtos!;
         sucessConection = true;
+        cadastrado = List<bool>.filled(listaProdutos.length, false);
       });
     }
   }
@@ -53,143 +57,154 @@ class _HomePageState extends State<HomePage> {
       }
     }
   }
-
   
   @override
   Widget build(BuildContext context) {
 
   final controller = context.watch<ProdutoController>();
-
-  print(listaProdutos.isEmpty);
     return Scaffold(
       appBar: AppBar(
         leading: Icon(Icons.menu),
         title: Text('Loja de Produtos'),
       ),
-      body: Visibility(
-        visible: sucessConection,
-        replacement: CircularProgressIndicator(),
-        child: SizedBox(
-          height: MediaQuery.of(context).size.height,
-          width: MediaQuery.of(context).size.width,
-          child: Container(
-            margin: EdgeInsets.all(10),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text('Disponivel: W\$ 368'),
-                    ElevatedButton(
-                      onPressed: (){
-                        Navigator.of(context).pushNamed('cadastro');
-                      }, 
-                      child: Text('Adicionar'))
-                  ],
-                ),
-                listaProdutos.isEmpty ? 
-                Container(color: Colors.purple, 
-                  child:Text('jajdi'),
-                ): Expanded(
-                  child: ListView.builder(
-                    itemCount: listaProdutos.length,
-                    itemBuilder: (context, index) {
-                      print(usuarioCadastrou());
-                      return Column(
-                        children: [
-                          Card(
-                            shape: RoundedRectangleBorder(
-                              side: BorderSide(
-                                color: Theme.of(context).colorScheme.primary,
+      body: Center(
+        child: Visibility(
+          visible: sucessConection,
+          replacement: CircularProgressIndicator(),
+          child: SizedBox(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            child: Container(
+              margin: EdgeInsets.all(10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text('Disponivel: W\$ ${controller.wsCoins}'),
+                      ElevatedButton(
+                        onPressed: (){
+                          Navigator.of(context).pushNamed('cadastro');
+                        }, 
+                        child: Text('Adicionar'))
+                    ],
+                  ),
+                  listaProdutos.isEmpty ? 
+                  Container(color: Colors.purple, 
+                    child:Text('jajdi'),
+                  ): Expanded(
+                    child: ListView.builder(
+                      itemCount: listaProdutos.length,
+                      itemBuilder: (context, index) {
+                        print(usuarioCadastrou());
+                        return Column(
+                          children: [
+                            Card(
+                              shape: RoundedRectangleBorder(
+                                side: BorderSide(
+                                  color: Theme.of(context).colorScheme.primary,
+                                ),
+                                borderRadius: BorderRadius.circular(10)
                               ),
-                              borderRadius: BorderRadius.circular(10)
-                            ),
-                            elevation: 7,
-                            child: Column(
-                              children: [
-                                Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: Row(
-                                    crossAxisAlignment: CrossAxisAlignment.center,
-                                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                                    children: [
-                                      Flexible(
-                                        flex: 1,
-                                        child: Image.asset('assets/images/fogao.jpeg'),
-                                      ),
-                                      Flexible(
-                                        flex: 1,
-                                        fit: FlexFit.tight,
-                                        child: Column(
-                                          children: [
-                                            Text('${listaProdutos[index].nome}', style: Theme.of(context).textTheme.titleLarge),
-                                            Text('${listaProdutos[index].descricao}', textAlign: TextAlign.center,),
-                                          ],
+                              elevation: 7,
+                              child: Column(
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Row(
+                                      crossAxisAlignment: CrossAxisAlignment.center,
+                                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                      children: [
+                                        Flexible(
+                                          flex: 1,
+                                          child: Image.asset('assets/images/fogao.jpeg')
                                         ),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                                cadastrado[index] ? Container(
-                                  margin: EdgeInsets.only(bottom: 10, right: 20, left: 20),
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(20),
-                                    color: Colors.green[400]
-                                  ),
-                                  width: double.infinity,
-                                  child: TextButton(
-                                    onPressed: () => showDialog(context: context, builder: (context) {
-                                      return AlertDialog(
-                                        title: Text('Confirma a aquisição deste produto?'),
-                                        actions: [
-                                          TextButton(
-                                            onPressed: ()=> Navigator.pop(context), 
-                                            child: Text('Cancelar')
+                                        Flexible(
+                                          flex: 1,
+                                          fit: FlexFit.tight,
+                                          child: Column(
+                                            children: [
+                                              Text('${listaProdutos[index].nome}', style: Theme.of(context).textTheme.titleLarge),
+                                              Text('${listaProdutos[index].descricao}', textAlign: TextAlign.center,),
+                                            ],
                                           ),
-                                          TextButton(
-                                            onPressed: (){}, 
-                                            child: Text('Sim')
-                                          ),
-                                        ],
-                                      );
-                                    },), 
-                                    child: Text('Adquirir por: w\$${listaProdutos[index].valor.toStringAsFixed(2)}', style: TextStyle(color: Colors.white),)
+                                        ),
+                                      ],
+                                    ),
                                   ),
-                                ) : Container(
-                                  margin: EdgeInsets.only(bottom: 10, right: 20, left: 20),
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(20),
-                                    color: Colors.red
+                                  cadastrado[index] ? Container(
+                                    margin: EdgeInsets.only(bottom: 10, right: 20, left: 20),
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(20),
+                                      color: Colors.red
+                                    ),
+                                    width: double.infinity,
+                                    child: TextButton(
+                                      onPressed: () => showDialog(context: context, builder: (context) {
+                                        return AlertDialog(
+                                          title: Text('Confirma a exclusão deste produto?'),
+                                          actions: [
+                                            TextButton(
+                                              onPressed: ()=> Navigator.pop(context), 
+                                              child: Text('Cancelar')
+                                            ),
+                                            TextButton(
+                                              onPressed: (){
+                                                controller.excluirProduto(index);
+                                                ScaffoldMessenger.of(context).showSnackBar(
+                                                  const SnackBar(content: Text('Produto excluido com sucesso'), backgroundColor: Colors.green,)
+                                                );
+                                                Navigator.pop(context);
+                                              }, 
+                                              child: Text('Sim')
+                                            ),
+                                          ],
+                                        );
+                                      },), 
+                                      child: Text('Exluir por: w\$${listaProdutos[index].valor.toStringAsFixed(2)}', style: TextStyle(color: Colors.white),)
+                                    ),
+                                  ) : Container(
+                                    margin: EdgeInsets.only(bottom: 10, right: 20, left: 20),
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(20),
+                                      color: Colors.green[400]
+                                    ),
+                                    width: double.infinity,
+                                    child: TextButton(
+                                      onPressed: () => showDialog(context: context, builder: (context) {
+                                        return AlertDialog(
+                                          title: Text('Confirma a aquisição deste produto?'),
+                                          actions: [
+                                            TextButton(
+                                              onPressed: ()=> Navigator.pop(context), 
+                                              child: Text('Cancelar')
+                                            ),
+                                            TextButton(
+                                              onPressed: (){
+                                                controller.adquirirProduto(index);
+                                                ScaffoldMessenger.of(context).showSnackBar(
+                                                  const SnackBar(content: Text('Adquirido com Sucesso'),backgroundColor: Colors.green,),
+                                                );
+                                                Navigator.pop(context);
+                                              }, 
+                                              child: Text('Sim')
+                                            ),
+                                          ],
+                                        );
+                                      },), 
+                                      child: Text('Adquirir por: w\$${listaProdutos[index].valor.toStringAsFixed(2)}', style: TextStyle(color: Colors.white),)
+                                    ),
                                   ),
-                                  width: double.infinity,
-                                  child: TextButton(
-                                    onPressed: () => showDialog(context: context, builder: (context) {
-                                      return AlertDialog(
-                                        title: Text('Confirma a aquisição deste produto?'),
-                                        actions: [
-                                          TextButton(
-                                            onPressed: ()=> Navigator.pop(context), 
-                                            child: Text('Cancelar')
-                                          ),
-                                          TextButton(
-                                            onPressed: (){}, 
-                                            child: Text('Sim')
-                                          ),
-                                        ],
-                                      );
-                                    },), 
-                                    child: Text('Exluir por: w\$${listaProdutos[index].valor.toStringAsFixed(2)}', style: TextStyle(color: Colors.white),)
-                                  ),
-                                ),
-                              ],
+                                ],
+                              ),
                             ),
-                          ),
-                        ],
-                      );
-                    },
-                  )),
-              ],
+                          ],
+                        );
+                      },
+                    )),
+                ],
+              ),
             ),
           ),
         ),
