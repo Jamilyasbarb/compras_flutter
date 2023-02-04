bd561cfa04c4d86658482c3570ae0409674bee15
Jamilyasbarb <mily.yasbarb@gmail.com>
2023-02-04 16:10:54 -0300

    feat: camera, imagem e validador
    adicionado logica para tirar uma foto e guardar ela dentro de widget para exibila. Modificada validacao do form

--- lib/pages/cadastro/cadastro_page.dart
+++ lib/pages/cadastro/cadastro_page.dart
@@ -1,5 +1,6 @@
 import 'package:compras_vita_health/controllers/produto_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:provider/provider.dart';
 
 class CadastroPage extends StatefulWidget {
@@ -12,7 +13,7 @@ class CadastroPage extends StatefulWidget {
 class _CadastroPageState extends State<CadastroPage> {
 
   final _keyForm = GlobalKey<FormState>();
-  int id = 55;
+
   @override
   Widget build(BuildContext context) {
     final controller = context.watch<ProdutoController>();
@@ -29,67 +30,86 @@ class _CadastroPageState extends State<CadastroPage> {
           margin: EdgeInsets.all(15),
           child: Column(
             children: [
-              Container(
-                margin: EdgeInsets.all(20),
-                height: 200,
-                width: 200,
-                color: Colors.grey[350],
-                child: Icon(Icons.camera_alt),
+              GestureDetector(
+                onTap: () => controller.pickerImage(ImageSource.camera),
+                child: controller.imagem == null ? Container(
+                  margin: EdgeInsets.all(20),
+                  height: 200,
+                  width: 200,
+                  color: Colors.grey[350],
+                  child: Icon(Icons.camera_alt),
+                ): Container(
+                  height: 200,
+                  width: 200,
+                  child: Image.file(controller.imagem!, fit: BoxFit.cover),
+                ),
               ),
               Expanded(
                 flex: 2,
-                child: Form(
-                  key: _keyForm,
-                  child: Column(
-                    children: [
-                      TextFormField(
-                        decoration: InputDecoration(
-                          labelText: 'Nome',
-                          border: OutlineInputBorder(),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SingleChildScrollView(
+                      child: Form(
+                        key: _keyForm,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            TextFormField(
+                              decoration: InputDecoration(
+                                labelText: 'Nome',
+                                border: OutlineInputBorder(),
+                              ),
+                              validator: (value) {
+                                if(value == null || value == ''){
+                                  return 'Campo obrigatório';
+                                }else{
+                                  controller.nome = value;
+                                }
+                                 return null;
+                              },
+                            ),
+                            Container(height: 10,),
+                            TextFormField(
+                              decoration: InputDecoration(
+                                labelText: 'Descrição',
+                                border: OutlineInputBorder(),
+                              ),
+                              validator: (value) {
+                                if(value == null || value == ''){
+                                   return 'Campo obrigatório';
+                                }else{
+                                  controller.descricao = value;
+                                }
+                                return null;
+                              },
+                            ),
+                            Container(height: 10,),
+                            TextFormField(
+                              decoration: InputDecoration(
+                                labelText: 'Valor em WsCoins',
+                                border: OutlineInputBorder(),
+                              ),
+                              validator: (value) {
+                                if(value == null || value == ''){
+                                  return 'Campo obrigatório';
+                                }else{
+                                  controller.valor = double.parse(value);
+                                }
+                                return null;
+                              },
+                            ),
+                          ],
                         ),
-                        validator: (value) {
-                          if(value != null && value != ''){
-                            controller.nome = value;
-                          }
-                           return null;
-                        },
                       ),
-                      Container(height: 10,),
-                      TextFormField(
-                        decoration: InputDecoration(
-                          labelText: 'Descrição',
-                          border: OutlineInputBorder(),
-                        ),
-                        validator: (value) {
-                          if(value != null && value != ''){
-                            controller.descricao = value;
-                          }
-                          return null;
-                        },
-                      ),
-                      Container(height: 10,),
-                      TextFormField(
-                        decoration: InputDecoration(
-                          labelText: 'Valor em WsCoins',
-                          border: OutlineInputBorder(),
-                        ),
-                        validator: (value) {
-                          if(value == null || value == ''){
-                            return 'Campo obrigatório';
-                          }else{
-                            controller.valor = double.parse(value);
-                          }
-                          return null;
-                        },
-                      ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
               ),
               Expanded(
                 flex: 1,
                 child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
                     Container(
                       width: double.infinity,
@@ -97,9 +117,9 @@ class _CadastroPageState extends State<CadastroPage> {
                       child: TextButton(
                         onPressed: (){
                           if(_keyForm.currentState!.validate()){
+                            controller.addProduto();
                             Navigator.of(context).pushReplacementNamed('/');
                           }
-                          controller.addProduto();
                         },
                         child: Text('Cadastrar', style: TextStyle(color: Colors.white),),
                       ),
